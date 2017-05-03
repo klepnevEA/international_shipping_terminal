d776390efd5abc1a827e9652b00e60403a9a0173
Klepnev <klepnev-ea@yandex.ru>
2017-05-03 12:23:05 +0300

    первая партия слайдов

--- source/js/app.js
+++ source/js/app.js
@@ -2,5 +2,32 @@
 
 $(document).ready(function() {
 
+	$('#datepicker').datepicker({
+    	orientation: "auto center",
+    	language: "ru",
+
+    	maxViewMode: 0
+	});
+
+	/*удаляем из календатря старые стрелки*/
+	$('.table-condensed .prev').html('');
+	$('.table-condensed .next').html('');
+
+	/*манипуляции с инпутом*/
+	$('.input__elem').on('focus', function() {
+		$(this).parent().addClass('active');
+	});
+
+	$('.input__elem').on('input', function() {
+		$(this).parent().children('.input__hint').addClass('active');
+	})
+
+	$('.input__elem').on('blur', function() {
+		if($(this).val() == '') {
+			$(this).parent().removeClass('active');
+		}
+		$(this).parent().children('.input__hint').removeClass('active');
+
+	});
 });
 
