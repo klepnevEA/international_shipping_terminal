2d8ac5c46f99792cbbd66224fb6334a1bc45d1b0
Klepnev <klepnev-ea@yandex.ru>
2017-05-10 18:36:33 +0300

    ,

--- source/js/app.js
+++ source/js/app.js
@@ -172,7 +172,7 @@ $(document).ready(function() {
 	updateDatePickerCells();
 	function updateDatePickerCells(dp) {
 		setTimeout(function () {
-			var cellContents = {10: '999 999', 13: '1 200', 15: '500', 17: '550', 19: '700', 28: '450'};
+			var cellContents = {10: '999 999', 13: '1 200', 15: '500',16: '600', 17: '550', 19: '700', 28: '450'};
 
 			$('.ui-datepicker td > *').each(function (idx, elem) {
 				var value = cellContents[idx + 1] || 0;
@@ -204,7 +204,7 @@ $(document).ready(function() {
 
 	/*клавиатура*/
    jQuery.keyboard.language.ru={language:"Русский (Russian)",display:{a:"✔:Сохранить2 (Shift+Enter)",accept:"Далее:Далее",alt:"РУС:Русская клавиатура",b:"←:Удалить символ слева",bksp:"⇦:Удалить символ слева",c:"✖:Отменить (Esc)",cancel:"Отменить:Отменить (Esc)",clear:"C:Очистить",combo:"ö:Toggle Combo Keys",dec:",:Decimal",e:"↵:Ввод",enter:"Ввод:Перевод строки",lock:"⇪ Lock:Caps Lock",s:"⇧:Верхний регистр",shift:"⇧:Верхний регистр",sign:"±:Сменить знак",space:"Пробел:",t:"⇥:Tab",tab:"⇥ Tab:Tab"},wheelMessage:"Use mousewheel to see other keys"};
-    jQuery.keyboard.layouts["ms-Russian"]={name:"ms-Russian",lang:["ru"],normal:["ё 1 2 3 4 5 6 7 8 9 0 - = {bksp}","{tab} й ц у к е н г ш щ з х ъ \\","ф ы в а п р о л д ж э {enter}","{shift} \\ я ч с м и т ь б ю / {shift}","{accept} {space} {cancel}"],shift:['Ё ! " № ; % : ? * ( ) _ + {bksp}',"{tab} Й Ц У К Е Н Г Ш Щ З Х Ъ /","Ф Ы В А П Р О Л Д Ж Э {enter}","{shift} / Я Ч С М И Т Ь Б Ю / {shift}","{accept} {space} {cancel}"]},jQuery.keyboard.layouts["ms-Russian (Typewriter)"]={name:"ms-Russian (Typewriter)",lang:["ru"],normal:['| № - / " : , . _ ? % ! ; {bksp}',"{tab} й ц у к е н г ш щ з х ъ )","ф ы в а п р о л д ж э {enter}","{shift} ) я ч с м и т ь б ю / {shift}","{accept} {space} {cancel}"],shift:["+ 1 2 3 4 5 6 7 8 9 0 = \\ {bksp}","{tab} Й Ц У К Е Н Г Ш Щ З Х Ъ (","Ф Ы В А П Р О Л Д Ж Э {enter}","{shift} ( Я Ч С М И Т Ь Б Ю / {shift}","{accept} {space} {cancel}"]};
+    jQuery.keyboard.layouts["ms-Russian"]={name:"ms-Russian",lang:["ru"],normal:["ё 1 2 3 4 5 6 7 8 9 0 - = {bksp}","{tab} й ц у к е н г ш щ з х ъ"," ф ы в а п р о л д ж э {enter}","{shift} \\ я ч с м и т ь б ю / {shift}","{accept} {space} {cancel}"],shift:['Ё ! " № ; % : ? * ( ) _ + {bksp}',"{tab} Й Ц У К Е Н Г Ш Щ З Х Ъ /","Ф Ы В А П Р О Л Д Ж Э {enter}","{shift} / Я Ч С М И Т Ь Б Ю / {shift}","{accept} {space} {cancel}"]},jQuery.keyboard.layouts["ms-Russian (Typewriter)"]={name:"ms-Russian (Typewriter)",lang:["ru"],normal:['| № - / " : , . _ ? % ! ; {bksp}',"{tab} й ц у к е н г ш щ з х ъ )","ф ы в а п р о л д ж э {enter}","{shift} ) я ч с м и т ь б ю / {shift}","{accept} {space} {cancel}"],shift:["+ 1 2 3 4 5 6 7 8 9 0 = \\ {bksp}","{tab} Й Ц У К Е Н Г Ш Щ З Х Ъ (","Ф Ы В А П Р О Л Д Ж Э {enter}","{shift} ( Я Ч С М И Т Ь Б Ю / {shift}","{accept} {space} {cancel}"]};
     $.keyboard.language.love = $.extend($.keyboard.language.ru);
     $('#from_element')
             .keyboard({
