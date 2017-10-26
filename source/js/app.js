0c80c1ab9680cdfb2fe90a30d37be311d278aeb9
vchernyaev <vchernyaev@usetech.ru>
2017-10-26 21:25:44 +0300

    added buy-37 and buy-38

--- source/js/app.js
+++ source/js/app.js
@@ -64,6 +64,15 @@ $(document).ready(function() {
 	    verticalSwiping: true
 	});
 
+	$(".tickets-back__slider").slick({
+	    dots: false,
+	    arrows: true,
+	    vertical: true,
+	    slidesToShow: 3,
+	    slidesToScroll: 1,
+	    verticalSwiping: true
+	});
+
 	/*с пересадкой / без пересадки*/
 
 	$('#checkbox').on('click', function () {
